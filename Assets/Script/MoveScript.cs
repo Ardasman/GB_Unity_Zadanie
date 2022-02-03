c3a7351e4ef23668f9ed9c9ccf06db6bcecad845
Ardasman <siberia_by@yahoo.com>
2022-02-03 18:53:50 +0300

    Второе задание
    Реализован базовый функционал

---
+++ Assets/Script/MoveScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Vector3 _movement;
+    Rigidbody _rigidbody;
+    Animator _animator;
+    Quaternion _rotation = Quaternion.identity;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        _movement.Set(horizontal, 0f, vertical);
+        _movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        _animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _movement, turnSpeed * Time.deltaTime, 0f);
+        _rigidbody.MovePosition(_rigidbody.position + _movement * _animator.deltaPosition.magnitude);
+        _rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        //_rigidbody.MovePosition(_rigidbody.position + _movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(_rotation);
+    }
+}
---
+++ Assets/Script/SpawnFirstAid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFirstAid : MonoBehaviour
+{
+    [SerializeField]  private float _spawnRangeX = 40f;
+    [SerializeField]  private float _spawnPosZ = 40f;
+
+    public GameObject[] _firstAidPrefabs;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, Random.Range(-_spawnPosZ, _spawnPosZ));
+            int firstAidIndex = Random.Range(0, _firstAidPrefabs.Length);
+            Instantiate(_firstAidPrefabs[firstAidIndex], spawnPos, _firstAidPrefabs[firstAidIndex].transform.rotation);
+            Destroy(gameObject, 0.38f);
+
+
+    }
+}
